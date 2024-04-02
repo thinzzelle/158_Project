44010b448f40e6adf820afd6b2488470fc17a825
thinzzelle <114443214+thinzzelle@users.noreply.github.com>
2024-04-01 18:42:53 -0700

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -3,7 +3,7 @@ from deepface import DeepFace
 # works with SFace, OpenFace, DeepID, Facenet and Facenet512
 result = DeepFace.verify(img1_path='img1.jpg', img2_path='img2.jpg', model_name='OpenFace')          #returns boolean if first pic matches second pic
 obj = DeepFace.analyze(img_path='img3.jpg', actions=['age', 'emotion', 'gender', 'race'])           #returns attribute to be printed out
-found = DeepFace.find(img_path='img3.jpg', db_path='Jessica_and_Angelina/', model_name='Facenet')                         #returns second location of second pic(s) matching the first pic
+found = DeepFace.find(img_path='img3.jpg', db_path='Jessica_and_Angelina/', model_name='Facenet')                         #returns location of second pic(s) matching the first pic
 
 #uncomment below to just print the location of found images
 #print(found[0]['identity'])
