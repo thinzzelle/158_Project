f4cc5342341f971da78739d487315c02011f4b11
Jason Whitlow <jason.whitlow45@gmail.com>
2024-04-07 22:41:11 -0700

    first test performed

--- face_similarity_tool.py
+++ face_similarity_tool.py
@@ -33,47 +33,38 @@
 from deepface import DeepFace
 
 # Function to count true positives, true negatives, false positives, and false negatives
-def _run_tests(model, detector, foler_size, pairs):
-    data_folder = 'rfw/test/data'
 
+def find_template_image(race, folder):
+    return 'rfw/test/data/' + race + '/' + folder + '/' + folder + '_0001.jpg'
+
+def find_test_image(race, folder, i):
+    file = '_000' + str(i) + '.jpg'
+    return 'rfw/test/data/' + race + '/' + folder + '/' + folder + file
+
+def _run_tests(race, model, detector, folder_size, lookup_table):
     tp, tn, fp, fn = 0, 0, 0, 0
     test_count = 0
 
-    num_photos = 1 #! ----Error
-    image_list = [] #! ------ERROR
+    # iterate through each folder
+    count = 0
+    for folder, size in folder_size:
+        template_image = find_template_image(race, folder)
 
-    with open(pairs, 'r') as f:
-        for line in f:
-            if test_count >= num_photos:  # Check a given number of photos
-                break
+        # run tests
+        for i in range(2, size+1):
+            test_image = find_test_image(race, folder, i)
+            print("test", test_image)
 
-            try:
-                img1_id, img1_label, img2_id, img2_label = line.strip().split('\t')
-                img1_path = [img_path for img_path, label in image_list if img1_id in img_path][0]
-                img2_path = [img_path for img_path, label in image_list if img2_id in img_path][0]
+            result = DeepFace.verify(template_image, test_image, model, detector)
+            print(result)
 
-                # Compare images
-                result = DeepFace.verify(img1_path, img2_path, model, detector)
-
-                if result['verified']:
-                    if img1_label == img2_label:
-                        tp += 1
-                    else:
-                        fp += 1
-                else:
-                    if img1_label == img2_label:
-                        fn += 1
-                    else:
-                        tn += 1
-
-                test_count += 1
-
-            except Exception as e:
-                print("Exception occurred:", str(e))
+        if count > 1:
+            break
+        count +=1
 
     return tp, tn, fp, fn, test_count
 
-def _init_folder_size_and_lookup_table(race):
+def _init_values(race):
     pairs_file_path = 'rfw/test/txts/' + race + '/' + race + '_pairs.txt'
     people_file_path = 'rfw/test/txts/' + race + '/' + race + '_people.txt'
 
@@ -87,12 +78,21 @@ def _init_folder_size_and_lookup_table(race):
     lookup_table = {}
     with open(pairs_file_path, 'r') as f:
         for line in f:
-            values = line.strip().split('\t')
+            data = line.strip().split('\t')
 
             # only accept pairs from the same file
-            if len(values) == 3:
-                print(values)
-                lookup_table[values[0]] = [int(values[1]),int(values[2])]
+            if len(data) == 3:
+                key, value = data[0], [int(data[1]), int(data[2])]
+                if key in lookup_table:
+                    lookup_table[key].append(value)
+                else:
+                    lookup_table[key] = [value]
+
+    # count = 1
+    # for key, value in lookup_table.items():
+    #     if len(value) > 1:
+    #         print(count, key, value)
+    #     count += 1
 
     return folder_size, lookup_table
 
@@ -101,36 +101,24 @@ def main():
     # races = ['African', 'Asian', 'Caucasian', 'Indian']
     races = ['African']
 
-    folder_size = []    # list of tuples containing folder name and number of people in folder
-    lookup_table = {}   # stores pairs from the same folder only
+    folder_size = []    # list of tuples. each tuple contains the folder name and number of people in folder
+    lookup_table = {}   # a dictionary to stores pairs from the same folder only.
 
     # DeepFace settings
     model = 'Facenet'
     detector = 'mtcnn'
 
     for race in races:
-
         print("Race:", race)
-
-        folder_size, lookup_table = _init_folder_size_and_lookup_table(race)
-        # print(folder_size)
-
-        # Assuming 'pairs' is your dictionary
-        count = 0
-        for key, value in lookup_table.items():
-            print(count, key, value)
-            count += 1
-
-
-        # tp, tn, fp, fn, test_count = _run_tests(model, detector, folder_size, pairs)
+        folder_size, lookup_table = _init_values(race)
+        tp, tn, fp, fn, test_count = _run_tests(race, model, detector, folder_size, lookup_table)
 
         # print("True Positives:", tp)
         # print("True Negatives:", tn)
         # print("False Positives:", fp)
         # print("False Negatives:", fn)
         # print("Total Tests:", test_count)
-        # print("Total People:", num_people)
-        # print()
+
 
 if __name__ == "__main__":
     main()
@@ -142,7 +130,7 @@ if __name__ == "__main__":
 
 
 #--------------- _find_folder_size_and_pairs
- # images_file_path = txt_folder + '/' + race + '/' + race + '_images.txt'
+# images_file_path = txt_folder + '/' + race + '/' + race + '_images.txt'
     # print(pairs_file, images_file_path, people_file_path)
 
     # Read image list
@@ -162,3 +150,32 @@ if __name__ == "__main__":
     #             break
     #
     # print(image_list)
+
+# with open(lookup_table, 'r') as f:
+#     for line in f:
+#         if test_count >= num_photos:  # Check a given number of photos
+#             break
+#
+#         try:
+#             img1_id, img1_label, img2_id, img2_label = line.strip().split('\t')
+#             img1_path = [img_path for img_path, label in image_list if img1_id in img_path][0]
+#             img2_path = [img_path for img_path, label in image_list if img2_id in img_path][0]
+#
+#             # Compare images
+#             result = DeepFace.verify(img1_path, img2_path, model, detector)
+#
+#             if result['verified']:
+#                 if img1_label == img2_label:
+#                     tp += 1
+#                 else:
+#                     fp += 1
+#             else:
+#                 if img1_label == img2_label:
+#                     fn += 1
+#                 else:
+#                     tn += 1
+#
+#             test_count += 1
+#
+#         except Exception as e:
+#             print("Exception occurred:", str(e))
