4c97791e8fb5e4b12eca66763e9b2bf5f57f2427
jason whitlow <jason.whitlow45@gmail.com>
2024-04-12 23:25:19 -0700

    add disimilarity_tool.py

--- face_disimilarity_tool.py
+++ face_disimilarity_tool.py
@@ -8,7 +8,7 @@ exception_write_to_file_count = 0
 
 
 def _write_exceptions_to_file(model):
-    filename = 'tmp/' + model + '/exceptions.txt'
+    filename = 'tmp2/' + model + '/exceptions.txt'
     with open(filename, 'w') as file:
         for exception in exception_list:
             file.write(str(exception) + '\n')
@@ -51,7 +51,7 @@ def _calculate_scores(race_metrics):
     return f1_score, accuracy, recall, precision, specificity
 
 def _write_final_results_to_file(races, metrics, model, detector):
-    filename = 'tmp/' + model + '/Race_results.txt'
+    filename = 'tmp2/' + model + '/Race_results.txt'
 
     try:
         with open(filename, 'w') as file:
@@ -82,12 +82,13 @@ def _write_final_results_to_file(races, metrics, model, detector):
         exception_list.append(exception_info)
     
 
-def _write_test_result_to_file(template_image_index, test_image_index, is_difficult_pair, result, folder, results_file, test_time):
+def _write_test_result_to_file(template_folder, template_image_index, test_folder, test_image_index, result, results_file, test_time):
     results_file.write(
-        f'{folder}\t\t'
+        f'{template_folder}\t\t'
+        f'{template_image_index}\t\t'
+        f'{test_folder}\t\t'
         f'{template_image_index}\t\t'
         f'{test_image_index}\t\t'
-        f'{int(is_difficult_pair)}\t\t'
         f'{int(result["verified"])}\t\t')
 
     if result['verified']:
@@ -140,19 +141,8 @@ def _get_test_image(race, folder, j):
     return 'rfw/test/data/' + race + '/' + folder + '/' + folder + file
 
 
-# returns true if template image and test image are the same person
-def _is_difiicult_pair(pair_list, template_image_index, test_image_index):
-    for pair in pair_list:
-        if pair[0] == template_image_index and pair[1] == test_image_index:
-            return True
-        elif pair[0] == test_image_index and pair[1] == template_image_index:
-            return True
-    return False
-
-
-def _calculate_test_result(is_difficult, result, race_metrics, test_time):
+def _calculate_test_result(result, race_metrics, test_time):
     
-    print("Is Difficult Pair?:\t", is_difficult)
     print("Model Prediction:", result['verified'])
 
     if result['verified']:
@@ -170,62 +160,48 @@ def _calculate_test_result(is_difficult, result, race_metrics, test_time):
     print("Total Test Count:\t", race_metrics['Total Test Count'])
 
 
-def _run_tests(race, model, detector, folder_size_list, lookup_table, metrics, folder_test_limit):
+def _run_tests(race, model, detector, folder_size_list, pair_list, metrics, test_limit):
     print("\n|||||||||| Race:", race, "||||||||||||||")
 
     global exeption_list
-
-    total_test_time = 0  # Variable to store the total test time
-    total_tests = 0  # Variable to store the total number of tests
-
     metrics_race = metrics[race]
 
     # Open results file for writing
-    with open(f'tmp/{model}/{race}_results.txt', 'w') as results_file:
+    with open(f'tmp2/{model}/{race}_results.txt', 'w') as results_file:
 
         # Write the header of results file
-        results_file.write('Folder\t\tTemplate\tTest\tDific?\tPredict\tResult\tTest Time\n')
+        results_file.write('Folder\t\tTemplate\tTest\tPredict\tResult\tTest Time\n')
 
-        # iterate through each folder
-        count = 1
-        for folder, size in folder_size_list:
+        # iterate through each image in the folder
+        count = 0
+        for pair in pair_list:
+            template_folder = pair[0]
+            template_index = pair[1]
+            template_image = _get_template_image(race, template_folder, template_index)
 
-            print("\n--------------- Testing Folder:", folder, "------------------")
-            print("Race:\t", race)
+            test_folder = pair[2]
+            test_index = pair[3]
+            test_image = _get_test_image(race, test_folder, test_index)
+            
+            print(f"\nTemplate:{template_image}\tTest:{test_image}")
 
-            # iterate through each image in the folder
-            for i in range(2, size + 1):
-                template_image_index = i
-                template_image = _get_template_image(race, folder, i)
+            try:
+                # run model
+                start_time = time.time()  # start the timer
+                result = DeepFace.verify(template_image, test_image, model, detector)
+                end_time = time.time()
+                test_time = end_time - start_time
+                tf.keras.backend.clear_session()
 
-                # run tests
-                for j in range(i+1, size + 1):
-                    test_image_index = j
-                    test_image = _get_test_image(race, folder, j)
-                    print(f"\nTemplate:{template_image}\tTest:{test_image}")
-
-                    pair_list = lookup_table[folder]
-
-                    try:
-                        # run model
-                        start_time = time.time()  # start the timer
-                        result = DeepFace.verify(template_image, test_image, model, detector)
-                        end_time = time.time()
-                        test_time = end_time - start_time
-                        tf.keras.backend.clear_session()
-
-                        # is the template and test the same person?
-                        is_dificult_pair = _is_difiicult_pair(pair_list, template_image_index, test_image_index)
-                        _calculate_test_result(is_dificult_pair, result, metrics_race, test_time)
-                        _write_test_result_to_file(template_image_index, test_image_index, is_dificult_pair, result, folder, results_file,  test_time)
-
-                    except Exception as e:
-                        print(str(e))
-                        exception_info = [str(e), race, count, folder, template_image,  test_image]
-                        exception_list.append(exception_info)
+                _calculate_test_result(result, metrics_race, test_time)
+                _write_test_result_to_file(template_folder, template_index, test_folder, test_index, result, results_file,  test_time)
+            except Exception as e:
+                print(str(e))
+                exception_info = [str(e), race, count, template_folder, template_image, test_image]
+                exception_list.append(exception_info)
 
             count += 1
-            if count > folder_test_limit:
+            if count > test_limit:
                 metrics_race['End Time'] = datetime.now()
                 break
 
@@ -240,20 +216,16 @@ def _init_values(race):
             group, num_people = line.strip().split('\t')
             folder_size.append((group, int(num_people)))
 
-    lookup_table = {}
+    pair_list = []
     with open(pairs_file_path, 'r') as f:
         for line in f:
             data = line.strip().split('\t')
 
-            # only accept pairs from the same file
-            if len(data) == 3:
-                key, value = data[0], [int(data[1]), int(data[2])]
-                if key in lookup_table:
-                    lookup_table[key].append(value)
-                else:
-                    lookup_table[key] = [value]
+            # only accept pairs from different files
+            if len(data) == 4:
+                pair_list.append(data[0], int(data[1], data[2]), int(data[3]))
 
-    return folder_size, lookup_table
+    return folder_size, pair_list
 
 
 def _init_metrics(race, metrics):
@@ -277,14 +249,14 @@ def main():
     model_list = ['Facenet512', 'Facenet']
     detector = 'mtcnn'
     metrics = {}
-    folder_test_limit = 3000
+    test_limit = 3000
     
     for model in model_list:
         for race in races:
-            folder_size_list, lookup_table = _init_values(race)
+            folder_size_list, pair_list = _init_values(race)
             _init_metrics(race, metrics)
 
-            _run_tests(race, model, detector, folder_size_list, lookup_table, metrics, folder_test_limit)
+            _run_tests(race, model, detector, folder_size_list, pair_list, metrics, test_limit)
 
         _print_results_to_console(races, metrics, model, detector)
         _write_final_results_to_file(races, metrics, model, detector)
