68a34004323c4fd56dbb9dce055374372b8346dd
Jason Whitlow <jason.whitlow45@gmail.com>
2024-04-21 14:54:59 -0700

    add charts to equal_error_rate.py

--- scripts/create_model_result_file.py
+++ scripts/create_model_result_file.py
@@ -1,6 +1,7 @@
 # calculates the race results for a set of files. The code reads the input file, processes the header and lines, and writes the output file. The code is executed for each file in the file_list list. The input and output paths are hard-coded in the script.
 from threshold_config import threshold_dict
 
+
 def _calculate_scores(tp, fp, tn, fn):
     test_count = tp + fp + tn + fn
 
@@ -34,7 +35,7 @@ def _calculate_scores(tp, fp, tn, fn):
     return f1_score, accuracy, recall, precision, specificity
 
 
-def write_results_to_file(race, threshold, input_file, output_file):    
+def write_results_to_file(race, threshold, input_file, output_file):
     # Read input file
     with open(input_file, 'r') as f_in:
         header = f_in.readline()
@@ -51,7 +52,7 @@ def write_results_to_file(race, threshold, input_file, output_file):
         distance = float(parts[7][:7])
         print(distance)
         print(threshold)
-        
+
         if parts[0] == parts[2] and distance < threshold:
             tp = tp + 1
         elif parts[0] == parts[2] and distance > threshold:
@@ -60,13 +61,14 @@ def write_results_to_file(race, threshold, input_file, output_file):
             fp = fp + 1
         elif parts[0] != parts[2] and distance > threshold:
             tn = tn + 1
-        
-        count +=1
+
+        count += 1
         if count > 100000:
             break
-    
+
     # Calculate accuracy
-    f1_score, accuracy, recall, precision, specificity = _calculate_scores(tp, fp, tn, fn)
+    f1_score, accuracy, recall, precision, specificity = _calculate_scores(
+        tp, fp, tn, fn)
 
     with open(output_file, 'w') as f_out:
         pass
@@ -74,34 +76,29 @@ def write_results_to_file(race, threshold, input_file, output_file):
     # Write output file
     with open(output_file, 'a') as f_out:
         f_out.write(race)
-        f_out.write(f"\n\tTrue Positive:\t{tp}\n\tTrue Negative:\t{tn}\n\tFale  Positive:\t{fp}\n\tFalse Negative:\t{fn}\n")
+        f_out.write(
+            f"\n\tTrue Positive:\t{tp}\n\tTrue Negative:\t{tn}\n\tFale  Positive:\t{fp}\n\tFalse Negative:\t{fn}\n")
         f_out.write(f"\tMatching Test Count:\t{tp + fn}\n")
         f_out.write(f"\tNon Matching Test Count:\t{tn + fp}\n")
         f_out.write(f"\tThreshold:\t{threshold}\n\n")
 
-        f_out.write(f"\tf1 score:\t{f1_score}\n\taccuracy:\t{accuracy}\n\trecall:\t\t{recall}\n\tprecision:\t{precision}\n\tspecificity:\t{specificity}\n\n")
+        f_out.write(
+            f"\tf1 score:\t{f1_score}\n\taccuracy:\t{accuracy}\n\trecall:\t\t{recall}\n\tprecision:\t{precision}\n\tspecificity:\t{specificity}\n\n")
+
 
 if __name__ == "__main__":
 
     # race_list = ["African", "Asian", "Caucasian", "Indian"]
     race_list = ["African"]
     model_list = ["DeepFace"]
-    model = model_list[0]
 
-    
-    # # theshold_json = os.getenv("THRESHOLD_DICT")
-    # theshold_dict = {
-    #     'DeepFace':0.35, 
-    #     'ArcFace':0.8, 
-    #     'Facenet': 0.4, 
-    #     'Facenet512': 0.4}
-    
-    path = "testing_results/verification/" + model + "/"
-    output_file = path + "Results.txt" 
-    
-    for race in race_list:
-        input_file = path + race + "_results.txt"
-        threshold = threshold_dict[model]
-        write_results_to_file(race, threshold, input_file, output_file)
-    print("Output file generated successfully.")
+    # NOTE: Model Thresholds are defined in threshold_config.py
+    for model in model_list:
+        path = "testing_results/verification/" + model + "/"
+        output_file = path + "Results.txt"
 
+        for race in race_list:
+            input_file = path + race + "_results.txt"
+            threshold = threshold_dict[model]
+            write_results_to_file(race, threshold, input_file, output_file)
+        print("Output file generated successfully.")
--- scripts/equal_error_rate.py
+++ scripts/equal_error_rate.py
@@ -1,27 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix, accuracy_score
+
 
 def read_data(file_path):
-    positive_pairs = []
-    negative_pairs = []
+    matching_pair_list = []
+    non_matching_pair_list = []
 
     with open(file_path, 'r') as file:
         file.readline()  # Skip the header
         count = 0
         for line in file:
             parts = line.split()
-            distance = float(parts[7][:7])  # Extracting and converting the distance to float
+            # Extracting and converting the distance to float
+            distance = float(parts[7][:7])
             if parts[0] == parts[2]:
-                positive_pairs.append(distance)
+                matching_pair_list.append(distance)
             else:
-                negative_pairs.append(distance)
+                non_matching_pair_list.append(distance)
 
             count += 1
             if count > 10000:
                 break
 
-    return positive_pairs, negative_pairs
+    return matching_pair_list, non_matching_pair_list
 
-def calculate_rates(positive_pairs, negative_pairs, thresholds):
+
+def calculate_rate_and_threshold(positive_pairs, negative_pairs, thresholds):
     fmr_list = []
     fnmr_list = []
     with open('tmp/temporary.txt', 'w') as file:
@@ -29,14 +34,20 @@ def calculate_rates(positive_pairs, negative_pairs, thresholds):
 
     min_value = 1
     for threshold in thresholds:
-        false_positive = sum(1 for distance in positive_pairs if distance > threshold)
+        false_positive = sum(
+            1 for distance in positive_pairs if distance > threshold)
         true_negative = len(positive_pairs) - false_positive
 
-        false_negative = sum(1 for distance in negative_pairs if distance < threshold)
+        false_negative = sum(
+            1 for distance in negative_pairs if distance < threshold)
         true_positive = len(negative_pairs) - false_negative
 
-        fmr = false_positive / (false_positive + true_negative) if (false_positive + true_negative) != 0 else 0
-        fnmr = false_negative / (false_negative + true_positive) if (true_positive + false_negative) != 0 else 0
+        fmr = false_positive / \
+            (false_positive + true_negative) if (false_positive +
+                                                 true_negative) != 0 else 0
+        fnmr = false_negative / \
+            (false_negative + true_positive) if (true_positive +
+                                                 false_negative) != 0 else 0
 
         # find the minimum value of fmr and fnmr
         if fmr >= fnmr and fmr < min_value:
@@ -47,46 +58,162 @@ def calculate_rates(positive_pairs, negative_pairs, thresholds):
             optimal_threshold = threshold
 
         fmr_list.append(fmr)
-        fnmr_list.append(fnmr)     
+        fnmr_list.append(fnmr)
 
     return fmr_list, fnmr_list, optimal_threshold
 
 
-def plot_rates(model, race, fpr_list, fnr_list, optimal_threshold, threshold_list):
-    plt.plot(threshold_list, fpr_list, label='False Positive Rate (FPR)')
-    plt.plot(threshold_list, fnr_list, label='False Negative Rate (FNR)')
-    plt.xlabel('Threshold')
-    plt.ylabel('Rate')
-    plt.title('Equal Error Rate (EER)')
-    plt.legend()
-    plt.xticks([i / 10 for i in range(11)])  # Set x-axis ticks at increments of 0.1
-    plt.grid(True)
-    plt.text(optimal_threshold -.39, 0.5, label='Optimal Threshold', s=f"Optimal Threshold: {optimal_threshold}", color='green')
+def calculate_accuracy(matrix):
+    return (matrix[0] + matrix[3]) / sum(matrix)
+
+
+def plot_rates(model, race_list, fmr_dict, fnmr_dict, optimal_threshold_dict, threshold_list):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+    axs = axs.flatten()
+
+    for i, race in enumerate(race_list):
+        axs[i].plot(threshold_list, fmr_dict[race],
+                    label='False Positive Rate (FPR)')
+        axs[i].plot(threshold_list, fnmr_dict[race],
+                    label='False Negative Rate (FNR)')
+        axs[i].set_xlabel('Threshold')
+        axs[i].set_ylabel('Rate')
+        axs[i].set_title(f'{model} - {race}')
+        axs[i].legend()
+        axs[i].grid(True)
+        axs[i].text(optimal_threshold_dict[race] + .15, 0.7,
+                    s=f"Optimal Threshold: {optimal_threshold_dict[race]}", color='green')
+
+    plt.tight_layout()
     plt.show()
 
+
+def calculate_tp_tn_fp_fn(matching_pair_list, non_matching_pair_list, optimal_threshold):
+    tp = sum(1 for distance in matching_pair_list if distance < optimal_threshold)
+    tn = sum(1 for distance in non_matching_pair_list if distance >
+             optimal_threshold)
+    fp = len(matching_pair_list) - tp
+    fn = len(non_matching_pair_list) - tn
+
+    return tp, tn, fp, fn
+
+
+def display_confusion_matrix(ax, matrix, accuracy):
+    labels = ['Positive', 'Negative', 'Positive', 'Negative']
+    values = np.array(matrix).reshape(2, 2)
+    ax.matshow(values, cmap='Greens')
+    for i in range(2):
+        for j in range(2):
+            ax.text(j, i, f'{values[i, j]}', ha='center', va='center')
+    ax.set_xticklabels([''] + labels[:2])
+    ax.set_yticklabels([''] + labels[2:])
+    ax.set_xlabel('Actual', weight='bold')
+    ax.set_ylabel('Predicted', weight='bold')
+    ax.xaxis.set_label_position('top')
+
+    ax.text(0.5, -0.1, f'Accuracy: {accuracy:.2f}', ha='center',
+            va='center', transform=ax.transAxes)
+
+
+def plot_confusion_matrices(model, race_list, matrix_dict, accuracy_dict):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+    axs = axs.flatten()
+
+    for i, race in enumerate(race_list):
+        display_confusion_matrix(
+            axs[i], matrix_dict[race], accuracy_dict[race])
+        axs[i].set_title(f'{model} - {race}', weight='bold')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_bar_chart(model_list, race_list, matrix_dict, accuracy_dict, optimal_threshold_dict):
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+    axs = axs.flatten()
+
+    for i, race in enumerate(race_list):
+        accuracies = [accuracy_dict[model][race] for model in model_list]
+        axs[i].bar(model_list, accuracies, color='skyblue')
+        axs[i].set_title(f'Accuracy Comparison - {race}')
+        axs[i].set_xlabel('Model')
+        axs[i].set_ylabel('Accuracy')
+        axs[i].set_ylim(0, 1)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_metrics(model, race_list, matrix_dict):
+    metrics = ['F1 Score', 'Accuracy', 'Precision', 'Recall', 'Specificity']
+    fig, axs = plt.subplots(len(metrics), figsize=(15, 20))
+
+    for j, metric in enumerate(metrics):
+        for k, race in enumerate(race_list):
+            tp, fp, fn, tn = matrix_dict[race]
+            accuracy = (tp + tn) / (tp + tn + fp + fn)
+            precision = tp / (tp + fp) if tp + fp != 0 else 0
+            recall = tp / (tp + fn) if tp + fn != 0 else 0
+            specificity = tn / (tn + fp) if tn + fp != 0 else 0
+            if metric == 'F1 Score':
+                f1_score = 2 * (precision * recall) / (precision +
+                                                       recall) if precision + recall != 0 else 0
+                axs[j].bar(race, f1_score, color='orange')
+            elif metric == 'Accuracy':
+                axs[j].bar(race, accuracy, color='skyblue')
+            elif metric == 'Precision':
+                axs[j].bar(race, precision, color='green')
+            elif metric == 'Recall':
+                axs[j].bar(race, recall, color='red')
+            elif metric == 'Specificity':
+                axs[j].bar(race, specificity, color='purple')
+            axs[j].set_title(f'{model} - {metric}')
+            axs[j].set_ylabel(metric)
+            axs[j].set_ylim(0, 1)
+            axs[j].set_xticks([])  # Remove x ticks
+
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
-    model_list = ['DeepFace', 'ArcFace', 'Facenet', 'Facenet512']
+    # model_list = ['DeepFace', 'ArcFace', 'Facenet', 'Facenet512']
+    model_list = ['DeepFace']
     race_list = ['African', 'Asian', 'Caucasian', 'Indian']
 
-    fmr_dict = {}
-    fnmr_dict = {}
-    optimal_threshold_dict = {}
-
     for model in model_list:
+        fmr_dict = {}
+        fnmr_dict = {}
+        matrix_dict = {}
+        accuracy_dict = {}
+        optimal_threshold_dict = {}
+
         for race in race_list:
             file_path = f'testing_results/verification/{model}/{race}_results.txt'
             threshold_list = [(i / 100) for i in range(101)]
 
             matching_pair_list, non_matching_pair_list = read_data(file_path)
-            fmr_list, fnmr_list, optimal_threshold = calculate_rates        (matching_pair_list, non_matching_pair_list, threshold_list)
+            fmr_list, fnmr_list, optimal_threshold = calculate_rate_and_threshold(
+                matching_pair_list, non_matching_pair_list, threshold_list)
+            tp, tn, fp, fn = calculate_tp_tn_fp_fn(
+                matching_pair_list, non_matching_pair_list, optimal_threshold)
 
             print(f"{model}, {race} Optimal threshold: {optimal_threshold}")
+            print(f"{model}, {race} TP: {tp}, TN: {tn}, FP: {fp}, FN: {fn}")
 
-            fmr_dict[race]= fmr_list
-            fnmr_dict[race]= fnmr_list
+            fmr_dict[race] = fmr_list
+            fnmr_dict[race] = fnmr_list
+            matrix_dict[race] = (tp, fp, fn, tn)
+            accuracy_dict[race] = calculate_accuracy(matrix_dict[race])
             optimal_threshold_dict[race] = optimal_threshold
 
-        plot_rates(model, race, fmr_dict, fnmr_dict, optimal_threshold_dict, threshold_list)
+        plot_rates(model, race_list, fmr_dict, fnmr_dict,
+                   optimal_threshold_dict, threshold_list)
+        plot_confusion_matrices(model, race_list, matrix_dict, accuracy_dict)
+
+        # plot_metrics(model, race_list, matrix_dict)
+    # plot_bar_chart(model_list, race_list, matrix_dict, accuracy_dict, optimal_threshold_dict)
+
 
 if __name__ == "__main__":
     main()
